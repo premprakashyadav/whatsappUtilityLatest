cc94f0e55f1f2a452caf20f2da3fc8fdb74d9b41
premprakashyadav <prem.sy89@gmail.com>
2026-01-03 23:03:34 +0530

    updated

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 /* ================= ADMIN AUTH ================= */
 
 function adminAuth(req, res, next) {
